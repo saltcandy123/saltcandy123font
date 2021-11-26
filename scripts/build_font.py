bea32d306463bee12dd59314e58c3800e907560f
saltcandy123 <saltcandy123@gmail.com>
2021-11-26 19:53:52 +0900

    Add "vert" subtable for vertical writing support
    CJK characters like hiragana will be added to saltcandy123font soon.
    This commit is a preparation for it as small hiragana characters have
    another style for vertical writing.

--- scripts/build_font.py
+++ scripts/build_font.py
@@ -5,6 +5,7 @@
 import argparse
 import pathlib
 import re
+import typing
 import xml.dom.minidom
 
 import fontforge
@@ -19,10 +20,27 @@ def build_saltcandy123font(*, version: str) -> fontforge.font:
     font.weight = "Regular"
     font.os2_weight = 400
     font.version = version
+    font.encoding = "UnicodeBmp"
+    font.hasvmetrics = 1  # required to enable vwidth settings
+
+    # Add lookup subtable for vertical writing
+    font.addLookup(
+        "gsub_vert",
+        "gsub_single",
+        (),
+        (
+            (
+                "vert",
+                (("DFLT", ("dflt",)),),
+            ),
+        ),
+    )
+    font.addLookupSubtable("gsub_vert", "vert")
 
     glyphs_dir = pathlib.Path(__file__).parent.parent.joinpath("glyphs")
     all_svg_paths = list(glyphs_dir.glob("**/*.svg"))
 
+    # Import basic glyph outlines (e.g. u3041.svg)
     for svg_path in all_svg_paths:
         match = re.search("^u([0-9a-f]{4}).svg$", svg_path.name)
         if not match:
@@ -30,9 +48,29 @@ def build_saltcandy123font(*, version: str) -> fontforge.font:
         code = int(match.group(1), 16)
         glyph = font.createChar(code)
         glyph.importOutlines(str(svg_path))
-        with xml.dom.minidom.parse(str(svg_path)) as doc:
-            glyph.width = int(doc.childNodes[0].getAttribute("width"))
+        glyph.width, glyph.vwidth = read_svg_size(svg_path)
+
+    # Import additional glyph outlines for subtable (e.g. u3041-vert.svg)
+    for svg_path in all_svg_paths:
+        match = re.search("^u([0-9a-f]{4})-([a-z]+).svg$", svg_path.name)
+        if not match:
+            continue
+        code = int(match.group(1), 16)
+        subtable_name = match.group(2)
+        glyph_name = f"u{code:04x}-{subtable_name}"
+        glyph = font.createChar(-1, glyph_name)
+        glyph.importOutlines(str(svg_path))
+        glyph.width, glyph.vwidth = read_svg_size(svg_path)
+        font[code].addPosSub(subtable_name, glyph_name)
+
     return font
+
+
+def read_svg_size(path_to_svg: pathlib.Path) -> typing.Tuple[int, int]:
+    with xml.dom.minidom.parse(str(path_to_svg)) as doc:
+        width = int(doc.childNodes[0].getAttribute("width"))
+        height = int(doc.childNodes[0].getAttribute("height"))
+        return width, height
 
 
 def main():
